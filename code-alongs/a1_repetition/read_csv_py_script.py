89f12b9e6cd0ac41dfb5c513f81d714139ecfc5c
unknown <nataliharju@gmail.com>
2025-08-19 15:27:32 +0200

    started excercises 0

---
+++ code-alongs/a1_repetition/read_csv_py_script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+
+# DATA_PATH = Path(__file__).parents[2] / "data"
+# df = pd.read_csv("../../data/Sales.csv", index_col=0, parse_dates=True)
+
+print("DATA_PATH")
+# print(df.head(5))
